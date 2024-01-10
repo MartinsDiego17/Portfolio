84213d48b1bcc27fd62b0ce7b328841c2c610091
Diego <martinsdiego0513@outlook.es>
2024-01-10 17:54:22 -0300

    Add paragraph and modify sub-titles

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -23,12 +23,26 @@ const About = () => {
     const { currentLenguaje } = useStoreLenguaje();
 
     const acerca = currentLenguaje === "es" ? "ACERCA DE MÍ" : "ABOUT ME";
+
+
     const parrafo1 = currentLenguaje === "es"
         ? "Apasionado del desarrollo web con incesante interés y curiosidad por este hermoso mundo siempre queriendo adquirir nuevos conocimientos y habilidades que me potencien en carrera."
+
         : "Passionate about web development with an insatiable interest and curiosity for this beautiful world, always eager to acquire new knowledge and skills that empower me in my career.";
 
-    const parrafo2 = currentLenguaje === "es" ? "En mi camino de aprendizaje, pude adquirir conocimientos sólidos en el campo, aportando en equipos de trabajo creando soluciones creativas y consistentes mayormente orientadas al front-end."
+
+    const parrafo2 = currentLenguaje === "es"
+        ? "En mi camino de aprendizaje, pude adquirir conocimientos sólidos en el campo, aportando en equipos de trabajo creando soluciones creativas y consistentes mayormente orientadas al front-end."
+
         : "In my learning journey, I was able to gain solid knowledge in the field, contributing to teamwork by creating creative and consistent solutions, primarily focused on the front-end.";
+
+    const parrafo3 = currentLenguaje === "es"
+        ? "A lo largo de mi vida he llevado a cabo disciplinas y actividades que fortalecieron muchos de mis aspectos personales como la disciplina, la constancia y la dedicación para cumplir cualquier objetivo."
+
+        : "Throughout my life, I have engaged in disciplines and activities that strengthened many aspects of my personality, such as discipline, perseverance, and dedication to achieve any goal.";
+
+
+
 
     const educacion = currentLenguaje === "es" ? "Educación" : "Education";
     const tecnologias = currentLenguaje === "es" ? "Tecnologías" : "Technologies";
@@ -46,6 +60,10 @@ const About = () => {
 
                 <p>
                     {parrafo2}
+                </p>
+
+                <p>
+                    {parrafo3}
                 </p>
 
                 <section>
